29de77d4df1be6253b6ccb25e72d6f6469eaabd6
Amit Kumar <email@amitkumar.com>
2021-05-24 21:06:36 -0400

    bump gulp to v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
-gulp.task('bundle', function() {
+gulp.task('bundle', (done) => {
   gulp.src('./src/loading.js')
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(gulp.dest('./dist/'));
+  done();
 });
